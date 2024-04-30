2a8a28f164a8168907bc016b407af7648b9a32b1
isaac <isaacantonioramosperez@alumno.ieselerincon.es>
2024-04-30 15:54:09 +0100

    defaultPage

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
+import Fighters from './pages/fighters/Fighters.jsx'
+import Contact from './pages/contact/Contact.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='*' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/contact' element={<Home/>}/>
+        <Route path='/fighters' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+
+  return (
+    <>
+    <h1>Hola</h1>
+      {/* <Header/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default Home
